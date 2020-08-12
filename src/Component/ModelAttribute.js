2ac4197efcc76be5c1625f62575c1413a14899c3
GeonwooVincentKim <kdsnop@gmail.com>
2020-08-12 11:09:09 +0900

    Added comments related to order of how does stateMachine works.

--- src/Component/ModelAttribute.js
+++ src/Component/ModelAttribute.js
@@ -6,10 +6,18 @@ import './ModelAttribute.css';
 
 // Define rules that govern what transitions will happend
 // based on which events that are triggered.
+
+// Order of executing Training-Model and discriminate images.
 const stateMachine = {
+    // First, genereate first-initial,
+    // and then generate five more after the initial-stage
+    // not named 'initial'.
     iniital: {},
-    initial: {},
-    initial: {},
+    loadingModel: {},
+    awaiting: {},  // Waiting for upload data(images).
+    ready: {},  // After waiting upload.
+    classifying: {},
+    complete: {},
 }
 
 const ModelAttribute = () => {
