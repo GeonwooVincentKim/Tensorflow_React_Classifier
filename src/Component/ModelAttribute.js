33491cf128c74ad7f1bbacfa1e0b23ea86bc8ab6
GeonwooVincentKim <kdsnop@gmail.com>
2020-08-12 11:30:47 +0900

    Successfully complete to create 'stateMachine'.

--- src/Component/ModelAttribute.js
+++ src/Component/ModelAttribute.js
@@ -10,21 +10,29 @@ import './ModelAttribute.css';
 // Order of executing Training-Model and discriminate images.
 const stateMachine = {
     /* 
+        - 1. How does construction of stateMachine-form looks like? 
         First, genereate first-initial,
         and then generate five more after the initial-stage
         not named 'initial'.
 
+        - 2. What does 'on: {}' do?
         Go to next-step naed loadingModel through 'on: {}'. 
         The event called next so in each of my state objects
         I need to specify which transition will happend based on the 
         next event.
+
+        - 3. Where does "complete: { on: {awaiting: ''}}," moves to?
+        If you are incomplete the next state you want to go is a waiting upload.
+        Because you kind of reset but the models ready already, so you want to await
+        the next-image to be classified, and then you're going to upload a picture of
+        another-dog.
     */
     iniital: { on: {next: 'loadingModel'} }, 
     loadingModel: { on: {loadingModel: 'awaiting'}},
     awaiting: { on: {awaiting: 'ready'}},  // Waiting for upload data(images).
     ready: { on: {awaiting: 'classifying'}},  // After waiting upload.
     classifying: { on: {awaiting: 'complete'}},
-    complete: { on: {awating: 'awaiting'}},
+    complete: { on: {awating: 'classifying'}}, 
 }
 
 const ModelAttribute = () => {
