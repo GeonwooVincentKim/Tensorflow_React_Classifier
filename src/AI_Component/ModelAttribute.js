712c2bbf8fdda8ca4fb389ad0a4c02ef1a66d1b3
GeonwooVincentKim <kdsnop@gmail.com>
2020-08-12 09:48:53 +0900

    Upgrading App.js and ModelLAttribute.js.

---
+++ src/AI_Component/ModelAttribute.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+import update from 'immutability-helper';
+import * as tf from '@tensorflow/tfjs';
+
+import './ModelAttribute.css';
+
+const ModelAttribute = () => {
+    
+}
--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+
+import ModelAttribute from './AI_Component/ModelAttribute';
+
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      
+        <ModelAttribute/>
     </div>
   );
-}
+};
 
 export default App;
