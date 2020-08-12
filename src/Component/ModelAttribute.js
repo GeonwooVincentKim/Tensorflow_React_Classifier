dabbe8679ec14bca52a66e0c30c3f8a19e02857f
GeonwooVincentKim <kdsnop@gmail.com>
2020-08-12 12:51:27 +0900

    Deleted unnecessary comments.

--- src/Component/ModelAttribute.js
+++ src/Component/ModelAttribute.js
@@ -9,24 +9,6 @@ import './ModelAttribute.css';
 
 // Order of executing Training-Model and discriminate images.
 const stateMachine = {
-    /* 
-        - 1. How does construction of stateMachine-form looks like? 
-        First, genereate first-initial,
-        and then generate five more after the initial-stage
-        not named 'initial'.
-
-        - 2. What does 'on: {}' do?
-        Go to next-step naed loadingModel through 'on: {}'. 
-        The event called next so in each of my state objects
-        I need to specify which transition will happend based on the 
-        next event.
-
-        - 3. Where does "complete: { on: {awaiting: ''}}," moves to?
-        If you are incomplete the next state you want to go is a waiting upload.
-        Because you kind of reset but the models ready already, so you want to await
-        the next-image to be classified, and then you're going to upload a picture of
-        another-dog.
-    */
     iniital: { on: {next: 'loadingModel'} }, 
     loadingModel: { on: {loadingModel: 'awaiting'}},
     awaitingUpload: { on: {awaiting: 'ready'}},  // Waiting for upload data(images).
@@ -40,6 +22,10 @@ const stateMachine = {
 // transitions to happen. 
 // For example, when I'd event gets fired. 
 // (I'm not sure this case is suitable to this comment..)
+
+const reducer = () => {
+
+}
 
 const ModelAttribute = () => {
     return (
